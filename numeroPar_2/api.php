327862bd956665d45ce410253b528ae1455b3487
JhonDavidHernandezCampus <yonif975@gmail.com>
2023-05-19 18:52:51 -0500

    Mensaje del commit

---
+++ numeroPar_2/api.php
@@ -0,0 +1,32 @@
+<?php
+    header("Content-Type: application/json;");
+    $_DATA = json_decode(file_get_contents("php://input"), true);
+    $METHOD = $_SERVER["REQUEST_METHOD"];
+    $numero = $_DATA["numero"];
+
+
+    try{
+        $res = match($METHOD){
+            "POST" => calcular(...$_DATA)
+        };
+    }catch(\Throwable $th){
+        $res = "Error";
+    }
+    function calcular(float $numero){
+        if(is_numeric($numero)){
+            if ($numero%2 == 0) {
+                if($numero > 10){
+                    echo "<h1>El numero es par y mayor a 10</h1>";
+                }else{
+                    echo "<h1>El numero es par y menor a 10</h1>";
+                }
+            }else{
+                if($numero > 10){
+                    echo "<h1>El numero es impar y mayor a 10</h1>";
+                }else{
+                    echo "<h1>El numero es impar y menor a 10</h1>";
+                }
+            }
+        }
+    }
+?>
---
+++ numeroPar_2/main.js
@@ -0,0 +1,21 @@
+
+let formulario = document.querySelector(".formulario");
+let header = new Headers({"Content-Type":"application/json"});
+let config ={
+    Headers : header,
+}
+
+formulario.addEventListener("submit", async (e)=>{
+    e.preventDefault();
+    config.method = "POST";
+    let data = Object.fromEntries(new FormData(e.target));
+    config.body = JSON.stringify(data);
+
+    let res = await(await fetch("api.php",config)).text();
+    console.log(JSON);
+    console.log(data);
+    console.log(config);
+    console.log(res);
+    document.querySelector("pre").innerHTML = res;
+
+})
