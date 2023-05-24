844d227718882729fd817e5e03e3b56e2ba94484
JhonDavidHernandezCampus <yonif975@gmail.com>
2023-05-23 19:31:06 -0500

    feat: :fire: Se agrego validacion en froned y se cambio el nomrbe de los archivos para mas organizacion

--- 1_notas/mail.js
+++ 1_notas/mail.js
@@ -9,11 +9,12 @@ let config ={
 
 formulario.addEventListener("submit", async (e)=>{
     e.preventDefault();
-    config.method = "POST";
+    config.method = "PO";
     let data = Object.fromEntries(new FormData(e.target));
     config.body = JSON.stringify(data);
     
     let res = await (await fetch ("api.php",config)).text();
+    console.log(res);
     document.querySelector("pre").innerHTML= res;
     console.log(document.querySelector("pre"));
 
